f4cfe1d0c0ca41649e2a596eba887d38b6c49088
Ali Mohammad Movahedian <am.movahedian1998@gmail.com>
2020-07-30 20:05:21 +0430

    Add sleep tracker

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepdetail/SleepDetailViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.android.trackmysleepquality.sleepdetail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import kotlinx.coroutines.Job
+
+class SleepDetailViewModel(
+        private val sleepNightKey : Long = 0L,
+        dataSource : SleepDatabaseDao) : ViewModel() {
+
+    val database = dataSource
+
+    private val job = Job()
+
+    private val night = MediatorLiveData<SleepNight>()
+
+    fun getNight() = night
+
+    init {
+        night.addSource(dataSource.getNightWithId(sleepNightKey), night::setValue)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+    val navigateToSleepTracker : LiveData<Boolean?>
+    get() = _navigateToSleepTracker
+
+    fun doneNavigating(){
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onClose(){
+        _navigateToSleepTracker.value = true
+    }
+
+}
---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepdetail/SleepDetailViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.example.android.trackmysleepquality.sleepdetail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import java.lang.IllegalArgumentException
+
+class SleepDetailViewModelFactory(
+        private val sleepNightKey : Long,
+        private val dataSource : SleepDatabaseDao) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepDetailViewModel::class.java)){
+            return SleepDetailViewModel(sleepNightKey, dataSource) as T
+        }
+        throw IllegalArgumentException("unknown viewmodel class")
+    }
+}
